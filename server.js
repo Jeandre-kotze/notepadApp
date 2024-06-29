4cfdeb791d50fa7576d32d5aa4e7c25922ebd3fb
Jeandre-kotze <Imonping7@gmail.com>
2024-06-29 19:13:14 +0200

    Updated bcrypt

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import pg from "pg";
 import env from "dotenv";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import bodyParser from "body-parser";
 import cors from "cors";
 
