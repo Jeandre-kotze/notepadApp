9f7c66c3f25c97c1c569a7a28bb0e9ebd954f346
Jeandre-kotze <Imonping7@gmail.com>
2024-07-12 18:47:37 +0200

    Fixed routing

--- src/RoutingPath.jsx
+++ src/RoutingPath.jsx
@@ -1,15 +1,21 @@
-
 import ErrorPage from "./components/ErrorPage.jsx";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
-const App = lazy(() => import("./components/App.jsx")) //Lazy loading, App doesnt need to be loaded so early
+
+// Lazy loading the App component
+const App = lazy(() => import("./components/App.jsx"));
 
 export default function RoutingPath() {
   return (
-  <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} errorElement={<errorElement/>}></Route>
-        <Route path='/notepad' element={<App />} errorElement={<errorElement/>}></Route>
-      </Routes>
-  </BrowserRouter>)}
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/notepad' element={<App />} />
+          <Route path='*' element={<ErrorPage />} /> {/* Catch-all route for errors */}
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
