6485be3dd46391491feeccc9512b495051a67a52
Sentinex <134302228+Jeandre-kotze@users.noreply.github.com>
2024-07-12 18:10:58 +0200

    Update RoutingPath.jsx

--- src/RoutingPath.jsx
+++ src/RoutingPath.jsx
@@ -3,8 +3,6 @@ import ErrorPage from "./components/ErrorPage.jsx";
 import { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
-import Login from "./components/Login";
-import Register from "./components/Register";
 const App = lazy(() => import("./components/App.jsx")) //Lazy loading, App doesnt need to be loaded so early
 
 export default function RoutingPath() {
@@ -12,8 +10,6 @@ export default function RoutingPath() {
   <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} errorElement={<errorElement/>}></Route>
-        <Route path='/login' element={<Login />} errorElement={<errorElement/>}></Route>
-        <Route path='/register' element={<Register />} errorElement={<errorElement/>}></Route>
         <Route path='/notepad' element={<App />} errorElement={<errorElement/>}></Route>
       </Routes>
   </BrowserRouter>)}
